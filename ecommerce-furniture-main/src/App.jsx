edddd56c9be6d966d658b4516fe7418ce8131642
rajveer <99086628+rajveer016@users.noreply.github.com>
2024-08-31 14:56:22 +0530

    Add files via upload

---
+++ ecommerce-furniture-main/src/App.jsx
@@ -0,0 +1,34 @@
+import './App.scss';
+import { Route, Routes } from 'react-router-dom';
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
+import Shop from "./pages/Shop/Shop";
+import Cart from "./pages/Cart/Cart";
+import NotFound from "./pages/NotFound/NotFound";
+import 'bootstrap/dist/css/bootstrap.css';
+import Product from './pages/Product/Product';
+import Connect from './components/Connect/Connect';
+import Contact from './components/Contact/Contact';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/shop/:id" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <Connect />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
